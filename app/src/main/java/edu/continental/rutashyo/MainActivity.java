a1d43fc8318e9319ab4b209438c85789b9c9e53c
abraham saldaña <abraham105@hotmail.com>
2020-10-06 22:22:56 -0500

    Controlador de botones user conductor

--- app/src/main/java/edu/continental/rutashyo/MainActivity.java
+++ app/src/main/java/edu/continental/rutashyo/MainActivity.java
@@ -2,13 +2,34 @@ package edu.continental.rutashyo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import edu.continental.rutashyo.Activity.LoginConductorActivity;
+import edu.continental.rutashyo.Activity.LoginUserActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnConductor, btnPasajero;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnConductor = findViewById(R.id.btnConductor);
+        btnPasajero = findViewById(R.id.btnPasajero);
+        btnPasajero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginUserActivity.class));
+            }
+        });
+        btnConductor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginConductorActivity.class));
+            }
+        });
     }
 }
