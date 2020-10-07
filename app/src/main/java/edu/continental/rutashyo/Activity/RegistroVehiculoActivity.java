a312a72727a35d9285a616c3cbc905e4d91eb3b7
GalvezTerrerosMauro <maurogaaalvezterreros@outlook.es>
2020-10-07 17:18:48 -0500

    Validacion de campos

--- app/src/main/java/edu/continental/rutashyo/Activity/RegistroVehiculoActivity.java
+++ app/src/main/java/edu/continental/rutashyo/Activity/RegistroVehiculoActivity.java
@@ -15,9 +15,10 @@ import edu.continental.rutashyo.R;
 
 public class RegistroVehiculoActivity extends AppCompatActivity {
 
+    private String valmarca,valmodelo,valcolor,valplaca;
     EditText edtmarca,edtmodelo,edtcolor,edtnum_placa;
     FloatingActionButton btnRegistrar;
-    private TextInputLayout input_layout_marca;
+    private TextInputLayout input_layout_marca,input_layout_modelo,input_layout_color,input_layout_placa;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,11 +30,18 @@ public class RegistroVehiculoActivity extends AppCompatActivity {
         btnRegistrar=findViewById(R.id.btnRegistrar);
 
         input_layout_marca=findViewById(R.id.input_layout_marca);
+        input_layout_modelo=findViewById(R.id.input_layout_modelo);
+        input_layout_color=findViewById(R.id.input_layout_color);
+        input_layout_placa=findViewById(R.id.input_layout_placa);
 
         btnRegistrar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(RegistroVehiculoActivity.this,InicioUserActivity.class));
+                valmarca=edtmarca.getText().toString();
+                valmodelo=edtmodelo.getText().toString();
+                valcolor=edtcolor.getText().toString();
+                valplaca=edtnum_placa.getText().toString();
+                enviarRegistro();
             }
 
         });
@@ -43,10 +51,20 @@ public class RegistroVehiculoActivity extends AppCompatActivity {
         if (!validarMarca()) {
             return;
         }
+        if (!validarModelo()) {
+            return;
+        }
+        if (!validarColor()) {
+            return;
+        }
+        if (!validarPlaca()) {
+            return;
+        }
+        registrarVehiculo();
     }
     private boolean validarMarca() {
         if (edtmarca.getText().toString().trim().isEmpty()) {
-            input_layout_marca.setError(getResources().getString(R.string.ingresa_tu_nombre));
+            input_layout_marca.setError(getResources().getString(R.string.campo_vacio));
             requestFocus(edtmarca);
             return false;
         } else {
@@ -55,10 +73,51 @@ public class RegistroVehiculoActivity extends AppCompatActivity {
 
         return true;
     }
+    private boolean validarModelo() {
+        if (edtmodelo.getText().toString().trim().isEmpty()) {
+            input_layout_modelo.setError(getResources().getString(R.string.campo_vacio));
+            requestFocus(edtmodelo);
+            return false;
+        } else {
+            input_layout_modelo.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validarColor() {
+        if (edtcolor.getText().toString().trim().isEmpty()) {
+            input_layout_color.setError(getResources().getString(R.string.campo_vacio));
+            requestFocus(edtcolor);
+            return false;
+        } else {
+            input_layout_color.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validarPlaca() {
+        if (edtnum_placa.getText().toString().trim().isEmpty()) {
+            input_layout_placa.setError(getResources().getString(R.string.campo_vacio));
+            requestFocus(edtnum_placa);
+            return false;
+        } else {
+            input_layout_placa.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
     private void requestFocus(View view) {
         if (view.requestFocus()) {
             getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
         }
+    }
+
+    public void registrarVehiculo(){
+
+        startActivity(new Intent(RegistroVehiculoActivity.this,InicioUserActivity.class));
     }
 
 
