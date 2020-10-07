a3c7c226d85517b3b5f33d80e333906c7eed7303
abraham saldaña <abraham105@hotmail.com>
2020-10-06 23:35:22 -0500

    TASK-8 creacion de modelo user

---
+++ app/src/main/java/edu/continental/rutashyo/model/user.java
@@ -0,0 +1,97 @@
+package edu.continental.rutashyo.model;
+
+public class user {
+    private String id;
+    private String nombres;
+    private String apellidos;
+    private String direccion;
+    private String fechaNacimiento;
+    private String nacionalidad;
+    private String telefono;
+    private String email;
+    private String contrasena;
+
+    public user(String id, String nombres, String apellidos, String direccion, String fechaNacimiento, String nacionalidad, String telefono, String email, String contrasena) {
+        this.id = id;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.fechaNacimiento = fechaNacimiento;
+        this.nacionalidad = nacionalidad;
+        this.telefono = telefono;
+        this.email = email;
+        this.contrasena = contrasena;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
