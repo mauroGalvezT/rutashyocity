60fe8b43da97f6c048d4439aadfc62f49b54db47
abraham saldaña <abraham105@hotmail.com>
2020-10-12 20:39:41 -0500

    menu bottom agregado

--- app/src/main/java/edu/continental/rutashyo/Activity/User/InicioUserActivity.java
+++ app/src/main/java/edu/continental/rutashyo/Activity/User/InicioUserActivity.java
@@ -3,16 +3,49 @@ package edu.continental.rutashyo.Activity.User;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import edu.continental.rutashyo.Activity.Fragments.HomeFragment;
+import edu.continental.rutashyo.Activity.Fragments.ProfileFragment;
 import edu.continental.rutashyo.R;
 
 public class InicioUserActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigation;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_user);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(new HomeFragment());
     }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.itemHome:
+                            openFragment(new HomeFragment());
+                            return true;
+                        case R.id.itemProfile:
+                            openFragment(new ProfileFragment());
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
