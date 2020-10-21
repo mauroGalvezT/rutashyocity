6326153030f25f7d293cf80467355269d5087ace
abraham saldaña <abraham105@hotmail.com>
2020-10-21 12:45:55 -0500

    update perfil

--- app/src/main/java/edu/continental/rutashyo/Activity/Fragments/ProfileFragment.java
+++ app/src/main/java/edu/continental/rutashyo/Activity/Fragments/ProfileFragment.java
@@ -1,8 +1,11 @@
 package edu.continental.rutashyo.Activity.Fragments;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,17 +14,39 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import dmax.dialog.SpotsDialog;
+import edu.continental.rutashyo.Activity.User.RegistroUserActivity;
 import edu.continental.rutashyo.R;
+import edu.continental.rutashyo.Retrofit.Respuesta.RespuestaRegistro;
+import edu.continental.rutashyo.Retrofit.SmartCityClient;
+import edu.continental.rutashyo.Retrofit.SmartCityService;
+import edu.continental.rutashyo.Retrofit.Solicitud.SolicitudUpdatePerfil;
 import edu.continental.rutashyo.settings.AppConst;
 import edu.continental.rutashyo.settings.SharedPreferencesManager;
 
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ProfileFragment extends Fragment {
     EditText edtCambiarNombre, edtCambiarApellido, edtCambiarTelefono, edtCambiarEmail, edtCambiarPassProfile;
     Button btnSave;
+    SmartCityService smartCityService;
+    SmartCityClient smartCityClient;
+    AlertDialog mDialog;
 
     public ProfileFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDialog = new SpotsDialog.Builder()
+                .setContext(getActivity())
+                .setMessage("Espere un momento")
+                .setCancelable(false).build();
     }
 
     @Override
@@ -34,7 +59,7 @@ public class ProfileFragment extends Fragment {
         edtCambiarTelefono = v.findViewById(R.id.edtCambiarTelefono);
         edtCambiarEmail = v.findViewById(R.id.edtCambiarEmail);
         edtCambiarPassProfile = v.findViewById(R.id.edtCambiarPassProfile);
-        btnSave = v.findViewById(R.id.btnSave);
+        btnSave = v.findViewById(R.id.btnSveUpdateProfile);
         String nombre = SharedPreferencesManager.getSomeStringValue(AppConst.PREF_USERNAMES);
         String apellido = SharedPreferencesManager.getSomeStringValue(AppConst.PREF_USERAPELLIDO);
         String telefono = SharedPreferencesManager.getSomeStringValue(AppConst.PREF_USERTELEFONO);
@@ -45,9 +70,70 @@ public class ProfileFragment extends Fragment {
         edtCambiarTelefono.setText(telefono);
         edtCambiarEmail.setText(email);
         edtCambiarPassProfile.setText(direccion);
+        retrofitInit();
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updatePerfil();
+
+            }
+        });
+
 
 
 
         return v;
+    }
+
+    private void updatePerfil() {
+        mDialog.show();
+        String idPerfil = SharedPreferencesManager.getSomeStringValue(AppConst.PREF_ID_USUARIO);
+        String name = edtCambiarNombre.getText().toString();
+        String apellido = edtCambiarApellido.getText().toString();
+        String direccion = edtCambiarPassProfile.getText().toString();
+        String nacimiento = "null";
+
+        String nacionalidad = "PERU";
+        String telefono = edtCambiarTelefono.getText().toString();
+        String email = edtCambiarEmail.getText().toString();
+
+        SolicitudUpdatePerfil solicitudUpdatePerfil = new SolicitudUpdatePerfil(idPerfil, name, apellido,direccion,nacimiento,nacionalidad,telefono,email);
+        Call<RespuestaRegistro> call = smartCityService.doUpdateProfile(solicitudUpdatePerfil);
+        call.enqueue(new Callback<RespuestaRegistro>() {
+            @Override
+            public void onResponse(Call<RespuestaRegistro> call, Response<RespuestaRegistro> response) {
+                if(response.isSuccessful()){
+                    mDialog.dismiss();
+                    Toast.makeText(getActivity(), "Datos guardados correctamente", Toast.LENGTH_SHORT).show();
+
+
+
+                }else{
+                    mDialog.dismiss();
+                    Toast.makeText(getActivity(), "Revise su datos", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RespuestaRegistro> call, Throwable t) {
+                mDialog.dismiss();
+                Toast.makeText(getActivity(), "Problemas de conexion", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+
+
+
+
+    }
+
+
+        private void retrofitInit() {
+        smartCityClient = SmartCityClient.getInstance();
+        smartCityService = smartCityClient.getSmartCityService();
+
     }
 }
